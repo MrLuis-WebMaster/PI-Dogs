3e1385ef5849470d56fccb75e6a0c03f84ed4fd0
Luis <MrLuis-WebMaster>
2022-02-06 11:00:30 -0500

    mejorando estilos nav

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/happy.png"
 import "./Nav.scss"
+
 export default function Nav () {
     return (
         <div className="flex">
